2ff6cc65f94c5f78d347c63a36346a8eb6c466b5
Ishan71845 <ishanbusiness1511@gmail.com>
2025-08-06 18:26:51 +0530

    Result Section Updated

--- Frontend/data/studentData.ts
+++ Frontend/data/studentData.ts
@@ -3,114 +3,192 @@
 export const studentPairs = [
   [
     {
-      name: "Bhushan Mishra",
+      name: "Vinay Ingle",
       exam: "JEE Advanced",
-      rank: "AIR 152",
-      image: "/students/Bankaras.png",
+      rank: "IIT Palakad - CSE",
+      image: "/results/student26.png",
     },
     {
-      name: "Hum Binod",
-      exam: "NEET UG",
-      rank: "Score: 695/720",
-      image: "/students/Binod.png",
+      name: "Siddharth Tribhuvan",
+      exam: "COEP, Pune",
+      rank: "PCMB - 94.4%",
+      image: "/results/student22.png",
+    },
+  ]
+  ,
+  [
+    {
+      name: "Saurab Saini",
+      exam: "IIT Jammu - CSE",
+      rank: "JEE Mains - 488/500",
+      image: "/results/student21.png",
+    },
+    
+      {
+      name: "Karthik Bankar",
+      exam: "JEE Mains - 96.5%ile",
+      rank: "488/500",
+      image: "/results/student13.png",
+    }, 
+  
+  ],
+  [
+    {
+      name: "Surjeet Gosh",
+      exam: "Army Public School, Pune",
+      rank: "PCMB - 94.4%",
+      image: "/results/student23.png",
+    },
+    {
+      name: "Tabish Ansari",
+      exam: "AISSMS, Pune",
+      rank: "12th 480/500",
+      image: "/results/student24.png",
     },
   ],
   [
     {
-      name: "Narendra Modi",
-      exam: "MHT CET",
-      rank: "99.8 percentile",
-      image: "/students/Modi.png",
+      name: "Vaishnavi Firode",
+      exam: "BDS, Bharti Vidyapeeth, Pune",
+      rank: "",
+      image: "/results/student25.png",
     },
     {
-      name: "Rahul Gandhi",
-      exam: "NDA",
-      rank: "AIR 42",
-      image: "/students/Raga.png",
-    },
-  ],
-  [
-    {
-      name: "M.K. Gandhi",
-      exam: "12th CBSE",
-      rank: "95.6%",
-      image: "/students/Gandhi.png",
-    },
-    {
-      name: "Nathuram Godse",
-      exam: "10th ICSE",
-      rank: "98.4%",
-      image: "/students/Godse.png",
+      name: "Karan Suryavanshi",
+      exam: "VJIT, Mumbai",
+      rank: "456/500",
+      image: "/results/student12.png",
     },
   ],
   [
     {
-      name: "Mamta",
-      exam: "JEE Main",
-      rank: "AIR 305",
-      image: "/students/Mamta.png",
+      name: "Khushboo Kumar",
+      exam: " NIMS, Delhi",
+      rank: "MBBS",
+      image: "/results/student14.png",
     },
     {
-      name: "Mayawati",
-      exam: "NEET UG",
-      rank: "Score: 682/720",
-      image: "/students/Mayawati.png",
+      name: "Aparna Shinde",
+      exam: "GMC, Nanded",
+      rank: "NEET 650/720",
+      image: "/results/student7.png",
+    }
+],
+  [
+    {
+      name: "Piyush Nindane",
+      exam: "COEP, Pune - CSE",
+      rank: "520/600",
+      image: "/results/student16.png",
+    },
+    {
+      name: "Rizwan Parveen",
+      exam: " 460/500",
+      rank: "Govt. College - West Bengal",
+      image: "/results/student19.png",
     },
   ],
   [
     {
-      name: "Tony Stark",
-      exam: "Foundation",
-      rank: "State Rank 3",
-      image: "/students/Tony.png",
+      name: "Ardra Thakare",
+      exam: "VJIT Mumbai, CSE",
+      rank: "451/500",
+      image: "/results/student8.png",
     },
     {
-      name: "Akshay Khanna",
-      exam: "Foundation",
-      rank: "State Rank 7",
-      image: "/students/Khanna.png",
+      name: "Ayesha Khalife",
+      exam: "Govt. College, Ratnagiri",
+      rank: "464/500",
+      image: "/results/student11.png",
     },
-  ],
+  ]
+  ,
+
   [
     {
-      name: "Tony Stark",
-      exam: "Foundation",
-      rank: "State Rank 3",
-      image: "/students/Tony.png",
+      name: "Mohd. Amaan",
+      exam: "CET - 475/500",
+      rank: "MIT Kothrud - CSE",  
+      image: "/results/student2.png",
     },
     {
-      name: "Akshay Khanna",
-      exam: "Foundation",
-      rank: "State Rank 7",
-      image: "/students/Khanna.png",
-    },
-  ],
-  [
-    {
-      name: "Tony Stark",
-      exam: "Foundation",
-      rank: "State Rank 3",
-      image: "/students/Tony.png",
-    },
-    {
-      name: "Akshay Khanna",
-      exam: "Foundation",
-      rank: "State Rank 7",
-      image: "/students/Khanna.png",
-    },
-  ],
-  [
-    {
-      name: "Tony Stark",
-      exam: "Foundation",
-      rank: "State Rank 3",
-      image: "/students/Tony.png",
-    },
-    {
-      name: "Akshay Khanna",
-      exam: "Foundation",
-      rank: "State Rank 7",
-      image: "/students/Khanna.png",
+      name: "Hridaya Prasantha",
+      exam: "490/500",
+      rank: "MIT Pune - CSE",
+      image: "/results/student18.png",
     },
   ],
 ];
+
+// {
+//     id: 6,
+//     name: "Aparna Shinde",
+//     result: "NEET 650/720",
+//     rank: "Govt. Medical College, Nanded",
+//     image: "/results/student7.png",
+//   },
+//   {
+//     id: 7,
+//     name: "Ardra Thakare",
+//     result: "451/500",
+//     rank: "VJIT Mumbai, CSE",
+//     image: "/results/student8.png",
+//   },
+//   {
+//     id: 10,
+//     name: "Ayesha Khalife",
+//     result: "464/500",
+//     rank: "Govt. College, Ratnagiri",
+//     image: "/results/student11.png",
+//   },
+//    {
+//       id: 11,
+//       name: "Karan Suryavanshi",
+//       result: "456/500",
+//       rank: "VJIT, Mumbai",
+//       image: "/results/student12.png",
+//     },
+//       {
+//       id: 12,
+//       name: "Karthik Bankar",
+//       result: "488/500",
+//       rank: "JEE Mains - 96.5%ile",
+//       image: "/results/student13.png",
+//     },
+//         {
+//       id: 12,
+//       name: "Khushboo Kumar",
+//       result: "MBBS",
+//       rank: "National Institute of Medical Sciences, Delhi",
+//       image: "/results/student14.png",
+//     },
+//        {
+//       id: 12,
+//       name: "Piyush Nindane",
+//       result: "520/600",
+//       rank: "COEP, Pune - CSE",
+//       image: "/results/student16.png",
+//     },
+//      {
+//       id: 12,
+//       name: "Saurab Saini",
+//       result: "JEE Mains - 488/500",
+//       rank: "IIT Jammu - CSE",
+//       image: "/results/student21.png",
+//     },
+    
+//     {
+//       id: 12,
+//       name: "Siddharth Tribhuvan",
+//       result: "",
+//       rank: "COEP, Pune",
+//       image: "/results/student22.png",
+//     },
+        
+//     {
+//       id: 12,
+//       name: "",
+//       result: "470/500",
+//       rank: "",
+//       image: "/results/student26.png",
+//     },
